75693909c66814518d9cce9e5b5ee7441a2313c2
Kolijn World <kolijn.wolfaardt@gmail.com>
2013-11-29 22:24:37 +0200

    Basic Eclipse project, Hello World code
    Signed-off-by: Kolijn World <kolijn.wolfaardt@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 PTGen
 =====
+
+A Procedural Texture generator in java, using lwjgl for display.
+
+http://lwjgl.org/
---
+++ src/com/PTGen/PTGen.java
@@ -0,0 +1,14 @@
+package com.PTGen;
+
+public class PTGen
+{
+	public PTGen ()
+	{
+		System.out.println("Starting");
+	}
+	
+	public static void main (String [] args)
+	{
+		PTGen theInstance = new PTGen();
+	}
+}
